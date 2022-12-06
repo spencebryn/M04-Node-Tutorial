6c40837f652f49cd9a542fb9f1f5bc948f7379a9
Spencer Strange <111928082+spencebryn@users.noreply.github.com>
2022-12-05 22:36:55 -0500

    update to m06

--- app.js
+++ app.js
@@ -1,19 +1,51 @@
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+const { render } = require('ejs');
+const dbURI = 'mongodb+srv://admin:11037@nodetuts.3eo4zkr.mongodb.net/nodetuts?retryWrites=true&w=majority'
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then((result) => {
+        app.listen(3000)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 
 const app = express();
 app.set('view engine', 'ejs')
 
-app.listen(3000);
+
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+app.use(morgan('dev'));
+
 
 app.get('/', (req, res) => {
-    const blogs = [
-        {title: 'The Batman Is The Best Movie This Year', snippet: 'This opinion is presented as fact'},
-        {title: 'I Wish The Blue Jackets Were Playing Better', snippet: 'Sometimes it is really hard to be a Columbus fan.'},
-        {title: 'RimWorld Is Addictive', snippet: 'There is just something about it where if I start playing I will be playing for 6 hours a day for the next month.'},
-    ]
-    // res.send('<p>home page</p>');
-    res.render('index', {title: 'Home', blogs});
+    res.redirect('/blogs');
 });
+
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({createdAt: -1})
+    .then((result) => {
+        res.render('index', {title: 'All Blogs', blogs: result})
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+})
+
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body);
+    blog.save()
+        .then((result) => {
+            res.redirect('/blogs');
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+})
 
 app.get('/about', (req, res) => {
     res.render('about', {title: 'About'});
@@ -22,6 +54,27 @@ app.get('/about', (req, res) => {
 app.get('/blogs/create', (req, res) => {
     res.render('create', {title: 'Create a new blog'});
 })
+
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    Blog.findById(id)
+        .then(result => {
+
+            res.render('details', {blog: result, title: 'Blog Details'})
+        })
+        .catch(err => {
+            console.log(err);
+        })
+})
+
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    Blog.findByIdAndDelete(id)
+        .then(result => {
+            res.json({ redirect: '/blogs'})
+        })
+})
+
 
 app.use((req, res) => {
     res.status(404).render('404', {title: '404'});
