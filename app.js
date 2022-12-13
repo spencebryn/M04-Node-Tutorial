506dcdaf4080af895f8c9177736f5edadd2568db
Spencer Strange <111928082+spencebryn@users.noreply.github.com>
2022-12-12 23:37:51 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog');
+
+const blogRoutes = require('./routes/blogRoutes');
 const { render } = require('ejs');
 const dbURI = 'mongodb+srv://admin:11037@nodetuts.3eo4zkr.mongodb.net/nodetuts?retryWrites=true&w=majority'
 mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
@@ -26,54 +27,13 @@ app.get('/', (req, res) => {
     res.redirect('/blogs');
 });
 
-app.get('/blogs', (req, res) => {
-    Blog.find().sort({createdAt: -1})
-    .then((result) => {
-        res.render('index', {title: 'All Blogs', blogs: result})
-    })
-    .catch((err) => {
-        console.log(err);
-    })
-})
-
-app.post('/blogs', (req, res) => {
-    const blog = new Blog(req.body);
-    blog.save()
-        .then((result) => {
-            res.redirect('/blogs');
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-})
+app.use('/blogs', blogRoutes);
 
 app.get('/about', (req, res) => {
     res.render('about', {title: 'About'});
 });
 
-app.get('/blogs/create', (req, res) => {
-    res.render('create', {title: 'Create a new blog'});
-})
 
-app.get('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findById(id)
-        .then(result => {
-
-            res.render('details', {blog: result, title: 'Blog Details'})
-        })
-        .catch(err => {
-            console.log(err);
-        })
-})
-
-app.delete('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findByIdAndDelete(id)
-        .then(result => {
-            res.json({ redirect: '/blogs'})
-        })
-})
 
 
 app.use((req, res) => {
